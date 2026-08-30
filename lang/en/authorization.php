1ce9bbab9a4e56b7a22128dbad8bd5d00181458b
TuanTQ <kutuanonline199@gmail.com>
2026-08-30 21:29:09 +0700

    feat(locate-fe): add files lang

---
+++ lang/en/authorization.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'denial' => [
+        'insufficient_permission' => 'You do not have permission to perform this action.',
+        'self_target' => 'You cannot modify your own authorization.',
+        'higher_privilege_target' => 'The target has higher effective privileges than you.',
+        'system_role' => 'System roles cannot be modified.',
+        'last_super_admin' => 'The last Super Admin must be preserved.',
+        'invalid_delegation' => 'This authorization delegation is not allowed.',
+    ],
+    'roles' => ['super_admin' => 'Super Admin', 'admin' => 'Admin', 'user' => 'User'],
+    'permissions' => [
+        'dashboard_view' => ['label' => 'View dashboard', 'group' => 'System'],
+        'users_view' => ['label' => 'View users', 'group' => 'Users'],
+        'users_manage' => ['label' => 'Manage users', 'group' => 'Users'],
+        'roles_view' => ['label' => 'View roles', 'group' => 'Roles'],
+        'roles_manage' => ['label' => 'Manage roles', 'group' => 'Roles'],
+        'permissions_view' => ['label' => 'View permissions', 'group' => 'Permissions'],
+        'permissions_manage' => ['label' => 'Synchronize permissions', 'group' => 'Permissions'],
+        'audit_view' => ['label' => 'View audit log', 'group' => 'Audit'],
+    ],
+];
