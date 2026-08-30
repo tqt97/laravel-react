babf871fd5acab3a8561d1abc1ec2a718478db8b
TuanTQ <kutuanonline199@gmail.com>
2026-08-30 21:28:41 +0700

    chore: improve logic and check type

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -17,12 +17,23 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $locale = $request->user()->locale->value
+        // Get the user's preferred locale
+        $candidate = $request->user()?->locale->value
             ?? $request->session()->get('locale')
             ?? config('app.locale', Locale::ENGLISH->value);
 
+        // Set the locale based on the user's preferred locale
+        $localeEnum = Locale::tryFrom((string) $candidate);
+        $locale = $localeEnum instanceof Locale
+            ? $localeEnum->value
+            : Locale::ENGLISH->value;
+
         App::setLocale($locale);
-        $request->session()->put('locale', $locale);
+
+        // Store the locale in the session if it has changed
+        if ($request->session()->get('locale') !== $locale) {
+            $request->session()->put('locale', $locale);
+        }
 
         return $next($request);
     }
